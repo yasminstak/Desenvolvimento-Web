952fe80e1a9e21187e6ff59436ffab373267f552
yasminstak <yasminstak@gmail.com>
2024-05-24 20:48:00 -0300

    listas

---
+++ listas/lista5/ex02/js/ex02.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome: ");
+var idade = prompt("Digite sua idade: ");
+var empregoAtual = prompt("Onde você trabalha atualmente?");
+var endereco = prompt("Qual seu endereço?");
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoAtual} \nSeu endereço é: ${endereco}`)
